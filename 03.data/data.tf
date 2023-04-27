2c82ebf6372f093244535447a6430cbc9d4c3848
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-27 18:17:10 +0530

    data

--- 03.data/data.tf
+++ 03.data/data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ami" {
 
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["258704684238"]
+  owners           = ["973714476881"]
 
 }
 
