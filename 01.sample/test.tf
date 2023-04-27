26366743d1f0c3fcd2c3998069fefe19a3a6f90a
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-27 13:18:30 +0530

    testing terraform

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
-  instance_type = "t3..micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-08d9f5a8a2fc2e8b4"]
   tags = {
     Name = "test"
