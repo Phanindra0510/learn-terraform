8ce24d5ef2e45e824d302004229cfcef26453bbd
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-05-10 17:10:14 +0530

    adding s3-state

--- 05.s3-state/main.tf
+++ 05.s3-state/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-ravi"
+    key    = "05.s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type= "t3.micro"
